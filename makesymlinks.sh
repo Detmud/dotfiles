9b37e762f7de6586aa774a83eafb8b97bec7451f
Detmud <detmud@me.com>
2014-03-12 19:06:58 +0100

    Added vim
    Changes to be committed:
    	modified:   makesymlinks.sh
    	new file:   vim/colors/smyck.vim

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="vimrc zlogin zlogout zpreztorc zprofile zshenv zshrc"    # list of files/folders to symlink in homedir
+files="vim vimrc zlogin zlogout zpreztorc zprofile zshenv zshrc"    # list of files/folders to symlink in homedir
 
 ##########
 
